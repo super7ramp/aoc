3304367f575e3166f2ee229371eb830c7851e095
Antoine Belvire <antoine.belvire@opensuse.org>
2024-12-05 19:05:09 +0100

    2024 day 5, part 2

--- 2024/05/Five.java
+++ 2024/05/Five.java
@@ -4,25 +4,30 @@ import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Predicate;
 
-interface Rule extends Predicate<List<Integer>> {}
+record Rule(Integer before, Integer after) implements Predicate<List<Integer>> {
+    @Override
+    public boolean test(final List<Integer> pages) {
+        final int afterIndex = pages.indexOf(after);
+        return afterIndex == -1 || pages.indexOf(before) < afterIndex;
+    }
+}
 
 static class Rules {
     private final Collection<Rule> rules;
+
     Rules(final Collection<Rule> rules) {
         this.rules = rules;
     }
+
     static Rules valueOf(final String input) {
         final String[] rulesToParse = input.split("\n");
         final var rules = new ArrayList<Rule>(rulesToParse.length);
-        for (final String rule : rulesToParse) {
-            final String[] parts = rule.split("\\|");
+        for (final String ruleToParse : rulesToParse) {
+            final String[] parts = ruleToParse.split("\\|");
             final Integer before = Integer.valueOf(parts[0]);
             final Integer after = Integer.valueOf(parts[1]);
-            final Rule predicate = pages -> {
-                final int afterIndex = pages.indexOf(after);
-                return afterIndex == -1 || pages.indexOf(before) < afterIndex;
-            };
-            rules.add(predicate);
+            final Rule rule = new Rule(before, after);
+            rules.add(rule);
         }
         return new Rules(rules);
     }
@@ -38,8 +43,27 @@ record Update(List<Integer> pages) {
         final List<Integer> pages = Arrays.stream(parts).map(Integer::valueOf).toList();
         return new Update(pages);
     }
+
     Integer middlePage() {
         return pages.get(pages.size() / 2);
+    }
+}
+
+record UpdateWithInfringedRules(Update update, List<Rule> infringedRules) {
+    Update correctedUpdate() {
+        if (infringedRules.isEmpty()) {
+            return update;
+        }
+        final List<Integer> correctedPages = new ArrayList<>(update.pages());
+        for (final Rule rule : infringedRules) {
+            final int beforeIndex = correctedPages.indexOf(rule.before);
+            final int afterIndex = correctedPages.indexOf(rule.after);
+            if (beforeIndex > afterIndex) {
+                correctedPages.remove(beforeIndex);
+                correctedPages.add(afterIndex, rule.before());
+            }
+        }
+        return new Update(correctedPages);
     }
 }
 
@@ -64,6 +88,35 @@ static class SafetyManual {
                 .filter(update -> rules.all().stream().allMatch(rule -> rule.test(update.pages())))
                 .toList();
     }
+
+    List<Update> correctedUpdates() {
+        final List<Update> nonValidUpdates = updates.stream()
+                .filter(update -> rules.all().stream().anyMatch(rule -> !rule.test(update.pages())))
+                .toList();
+        return correct(nonValidUpdates);
+    }
+
+    private List<Update> correct(final List<Update> updatesToCorrect) {
+        final List<UpdateWithInfringedRules> updatesWithInfringedRules = updatesToCorrect.stream()
+                .map(update -> {
+                    final List<Rule> infringedRules = rules.all().stream()
+                            .filter(rule -> !rule.test(update.pages()))
+                            .toList();
+                    return new UpdateWithInfringedRules(update, infringedRules);
+                })
+                .toList();
+
+        final List<Update> correctedUpdates = updatesWithInfringedRules.stream()
+                .map(UpdateWithInfringedRules::correctedUpdate)
+                .toList();
+
+        if (correctedUpdates.stream().anyMatch(correctedUpdate -> rules.all().stream().anyMatch(rule -> !rule.test(correctedUpdate.pages())))) {
+            System.out.println("Correction is incomplete, iterating");
+            return correct(correctedUpdates);
+        }
+
+        return correctedUpdates;
+    }
 }
 
 void main() throws IOException {
@@ -71,10 +124,17 @@ void main() throws IOException {
     final SafetyManual manual = SafetyManual.valueOf(input);
 
     final List<Update> validatedUpdates = manual.validatedUpdates();
-    System.out.println("Validated updates: " + validatedUpdates);
+    System.out.println("(Part 1) Validated updates: " + validatedUpdates);
 
     final int validatedUpdatesMiddlePageSum = validatedUpdates.stream()
             .mapToInt(Update::middlePage)
             .reduce(0, Integer::sum);
-    System.out.println("Sum of validated updates middle pages: " + validatedUpdatesMiddlePageSum);
+    System.out.println("(Part 1) Sum of validated updates middle pages: " + validatedUpdatesMiddlePageSum);
+
+    final List<Update> correctedUpdates = manual.correctedUpdates();
+    System.out.println("(Part 2) Corrected updates: " + correctedUpdates);
+    final int correctedUpdatesMiddlePageSum = correctedUpdates.stream()
+            .mapToInt(Update::middlePage)
+            .reduce(0, Integer::sum);
+    System.out.println("(Part 2) Sum of corrected updates middle pages: " + correctedUpdatesMiddlePageSum);
 }
