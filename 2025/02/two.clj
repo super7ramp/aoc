be5dd656fbb49ba7b826d0f21457546c888d18af
Antoine Belvire <antoine.belvire@opensuse.org>
2025-12-04 13:15:03 +0100

    polish

--- 2025/02/two.clj
+++ 2025/02/two.clj
@@ -1,8 +1,8 @@
 (ns two
   (:require [clojure.string :as str]))
 
-(defn- parse-range [range-str]
-  (let [[start end] (->> (str/split range-str #"-")
+(defn- parse-range [range-string]
+  (let [[start end] (->> (str/split range-string #"-")
                          (map parse-long))]
     (range start (inc end))))
 
@@ -10,28 +10,32 @@
   (->> (str/split input #",\n?")
        (map parse-range)))
 
-(defn- has-repeated-substrings? [string times]
-  (if (not (zero? (rem (count string) times)))
-    false
-    (let [part-length (quot (count string) times)
-          parts (->> (range times)
-                     (map #(subs string (* % part-length) (* (inc %) part-length))))]
-      (apply = parts))))
+(defn- has-repeated-substring? [string times]
+  (let [string-length (count string)]
+    (if (not (zero? (rem string-length times)))
+      false
+      (let [substring-length (quot string-length times)
+            substrings (->> (range times)
+                            (map #(subs string
+                                        (* % substring-length)
+                                        (* (inc %) substring-length))))]
+        (apply = substrings)))))
 
 (defn is-invalid-id? [id]
-  (has-repeated-substrings? (str id) 2))
+  (has-repeated-substring? (str id) 2))
 
 (defn is-really-invalid-id? [id]
   (let [id-string (str id), id-string-length (count id-string)]
     (loop [times 2]
       (cond
-        (has-repeated-substrings? id-string times) true
-        (< times id-string-length) (recur (inc times))))))
+        (has-repeated-substring? id-string times) true
+        (< times id-string-length) (recur (inc times))
+        :else false))))
 
 (def input (slurp "input.txt"))
 
-(let [ranges (flatten (parse-ranges input))]
+(let [ids (flatten (parse-ranges input))]
   (println "Part 1:")
-  (println "Invalid ID sum: " (time (->> ranges (filter is-invalid-id?) (reduce +))))
+  (println "Invalid ID sum: " (time (->> ids (filter is-invalid-id?) (reduce +))))
   (println "Part 2:")
-  (println "Invalid ID sum: " (time (->> ranges (filter is-really-invalid-id?) (reduce +)))))
+  (println "Invalid ID sum: " (time (->> ids (filter is-really-invalid-id?) (reduce +)))))
